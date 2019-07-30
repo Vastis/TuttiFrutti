ceaeb9eb9c3a1dc50c16340f56871d17c1589aa2
Bartosz Jaworski <50333809+Aveane@users.noreply.github.com>
2019-07-30 16:26:57 +0200

    Read connection config from file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import client.Client;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args){
+
+        new Client().start();
 
     }
 }
