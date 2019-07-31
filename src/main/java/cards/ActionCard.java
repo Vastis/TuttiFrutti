988ac18710fde548ed1dc6bdca90e598ccff4b2e
Vastis <daniel.wiechecki@gmail.com>
2019-07-31 12:25:02 +0200

    Actions template

--- src/main/java/cards/ActionCard.java
+++ src/main/java/cards/ActionCard.java
@@ -1,5 +1,7 @@
 package cards;
 
+import actions.Action;
+
 public class ActionCard extends Card {
     enum actionCategory {
         TERRAIN,
@@ -10,10 +12,18 @@ public class ActionCard extends Card {
     private actionCategory category;
     private String name;
 
+    private Action action;
+
     public ActionCard(cardType type, actionCategory category, String name) {
         super(type);
         this.category = category;
         this.name = name;
+        this.action = initAction();
+    }
+
+    private Action initAction() {
+        //TODO
+        return null;
     }
 
     public actionCategory getCategory() {
@@ -22,5 +32,9 @@ public class ActionCard extends Card {
 
     public String getName() {
         return name;
+    }
+
+    public Action getAction(){
+        return this.action;
     }
 }
